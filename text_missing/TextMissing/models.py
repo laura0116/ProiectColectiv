ab7f0e65c0b54bb16c96c58de85266ba845f50cb
Laura Matei <mti.laura01@gmail.com>
2016-12-14 22:32:12 +0200

    Created model for document

--- text_missing/TextMissing/models.py
+++ text_missing/TextMissing/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from text_missing.LoginApp.models import Client
+
+class StatusChoices:
+    DRAFT = 'Draft'
+    FINAL = 'Final'
+    FINAL_REVISED = 'Final revised'
+    BLOCKED = 'Blocked'
+    CHOICES = [(DRAFT,FINAL,FINAL_REVISED,BLOCKED)]
+
+class Document(models.Model):
+    documentName = models.CharField(max_length=64)
+    author = models.ForeignKey(Client.type("contributor", "admin", "manager"), null=False, default=1)
+    size = models.IntegerField(default=0)
+    version = models.CharField(max_length=64)
+    creation_date = models.DateField()
+    last_update = models.DateField()
+    abstract = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=100)
+    status = models.CharField(max_length=20,choices=StatusChoices.CHOICES)
+
+
+
+
