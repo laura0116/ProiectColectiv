007a79e7dbff10b961039e0660d40066a838471e
Uifalean Sergiu <usergiu95@gmail.com>
2016-12-16 21:23:58 +0200

    Added bootstrap, added delete functionality for delete.(not in the scope,but it will help).

--- text_missing/TextMissing/urls.py
+++ text_missing/TextMissing/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "TextMissing"
 urlpatterns = [
-    url(r'^documents/', views.documents_page, name='documents'),
+    url(r'^documents/$', views.documents_page, name='documents'),
+    url(r'^documents/delete/(?P<document_id>.*)', views.delete_document, name='delete_document'),
 ]
--- text_missing/TextMissing/views.py
+++ text_missing/TextMissing/views.py
@@ -13,3 +13,11 @@ def documents_page(request):
         return redirect('admin:index')
     return render(request, "TextMissing/documents.html",
                   {'documents': Document.objects.all(), "has_permission": True})
+
+
+@login_required(login_url=reverse_lazy('LoginApp:login'))
+def delete_document(request, document_id):
+    print(request.method)
+    if request.method == "GET":
+        Document.objects.filter(id=document_id).delete()
+    return redirect('TextMissing:documents')
