0ee92294f20643dbfe1d09b840570c9849cc3c2d
Sergiu Uifalean <sergiu.uifalean@tss-yonder.com>
2017-01-21 13:06:30 +0200

    add repeated running task executor

--- text_missing/TextMissing/utils/emailer.py
+++ text_missing/TextMissing/utils/emailer.py
@@ -2,20 +2,19 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-class Emailer:
-    def __init__(self):
-        pass
 
-    def send_mail(self, destination, message, subject = "Test subject"):
+class Emailer:
+    @staticmethod
+    def send_mail(destination, message, subject="No subject"):
         fromaddr = "text.missing.info"
         toaddr = destination
         msg = MIMEMultipart()
         msg['From'] = fromaddr
         msg['To'] = toaddr
-        msg['Subject'] = "Email Test Subject"
+        msg['Subject'] = subject
 
         body = message
-        msg.attach(MIMEText(body, 'plain'))
+        msg.attach(MIMEText(body, 'HTML'))
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
